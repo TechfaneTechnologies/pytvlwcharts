5f8cdd1ba439c162eae2674d60020226a9ac6ae7
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2022-09-03 04:58:56 +0530

    Create __version__.py

---
+++ pytvlwcharts/__version__.py
@@ -0,0 +1,8 @@
+__title__ = "pytvlwcharts"
+__description__ = "An Experimental Python Wrapper For Tradingview's Lightweight-Charts To Be Used In Notebook Environments."
+__url__ = "https://github.com/TechfaneTechnologies"
+__download_url__ = "https://github.com/TechfaneTechnologies/pytvlwcharts"
+__version__ = "0.0.1"
+__author__ = "Techfane Technology Pvt. Ltd. (India)"
+__author_email__ = "moonedrjune@gmail.com"
+__license__ = ("MIT", "Apache-2.0")
