0e18a2e414e843e724a5a836f66e155b671e25e8
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2022-09-03 05:05:35 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,17 +1,58 @@
-import setuptools
+#!/usr/bin/env python
 
-setuptools.setup(
-    name='pytvlwcharts',
-    version='0.0.1',
-    description=
-    "An Experimental Python Wrapper For Tradingview's Lightweight-Charts To Be Used In Notebook Environments.",
-    long_description=open('README.md', 'r', encoding='utf8').read(),
-    long_description_content_type="text/markdown",
-    url='https://github.com/TechfaneTechnologies/pytvlwcharts',
-    project_urls={
-        "Bug Tracker": "https://github.com/TechfaneTechnologies/pytvlwcharts/issues"
-    },
-    license='MIT License',
-    packages=['pytvlwcharts'],
-    install_requires=['apischema', 'jinja2', 'absl-py', 'pandas', 'numpy'],
+import io
+import os
+from codecs import open
+from setuptools import setup
+
+current_dir = os.path.abspath(os.path.dirname(__file__))
+
+about = {}
+with open(os.path.join(current_dir, "pytvlwcharts", "__version__.py"), "r", "utf-8") as f:
+    exec(f.read(), about)
+
+with io.open('README.md', 'rt', encoding='utf8') as f:
+    readme = f.read()
+    
+setup(
+    name=about["__title__"],
+    version=about["__version__"],
+    description=about["__description__"],
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    author=about["__author__"],
+    author_email=about["__author_email__"],
+    url=about["__url__"],
+    download_url=about["__download_url__"],
+    license=about["__license__"],
+    packages=["pytvlwcharts"],
+    classifiers=[
+        "Development Status :: 1 - Development/Alpha",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Financial and Insurance Industry",
+        "Programming Language :: Python",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Office/Business :: Financial :: Investment",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "Topic :: Software Development :: Libraries"
+    ],
+    install_requires=[
+        'apischema',
+        'jinja2',
+        'absl-py',
+        'pandas',
+        'numpy'
+    ],
+    extras_require={
+        "doc": ["pdoc"],
+        ':sys_platform=="win32"': ["pywin32"]
+    }
 )
