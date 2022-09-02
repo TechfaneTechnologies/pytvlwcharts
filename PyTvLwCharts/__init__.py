07426cdf149e55694afa6646b3f722e4b393a060
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2022-09-03 02:11:10 +0530

    Update __init__.py

--- PyTvLwCharts/__init__.py
+++ PyTvLwCharts/__init__.py
@@ -1,2 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+    __init__.py
+    An Experimental Python Wrapper For Tradingview's Lightweight-Charts
+    :url: https://tradingview.github.io/lightweight-charts/
+    :copyright: (c) 2021 by Techfane Technologies Pvt. Ltd.
+    :license: see LICENSE for details.
+    :author: Dr June Moone
+    :created: On Friday September 02, 2022 19:47:13 GMT+05:30
+"""
+__author__ = "Dr June Moone"
+__webpage__ = "https://github.com/MooneDrJune"
+__github__ = "https://github.com/TechfaneTechnologies/PyTvLwCharts"
+__license__ = "MIT"
+
 from .tvlwcharts import Chart
 from .generated_models import *
