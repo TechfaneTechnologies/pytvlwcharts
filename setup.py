e6d7d9fe0c63a80e4b8b1159d18119cd2adf9f3c
DrJuneMoone <96371033+MooneDrJune@users.noreply.github.com>
2023-02-21 01:55:22 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -38,6 +38,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Office/Business :: Financial :: Investment",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Software Development :: Libraries"
